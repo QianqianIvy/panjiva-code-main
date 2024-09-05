cc971665e16d39bbcb468dac239ae8404de35bee
Qianqianivy <tangfdu@gmail.com>
2024-09-05 19:41:03 -0400

    delete unnecessary stuff

--- code/fig.R
+++
@@ -1,8 +0,0 @@
-# import package for plots, reading csv files
-library(ggplot2)
-library(readr)
-
-# read csv file delay_days.csv
-delay_days <- read_csv("/Users/qianqiantang/Desktop/panjiva-code-main/Processed_data/USImport/delay_days.csv") #nolint
-# show the first few rows of the data
-head(delay_days)
