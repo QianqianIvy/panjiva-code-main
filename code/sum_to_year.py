ab49fbe0837c91bf3ba2cb6c191947f294e677ad
Qianqianivy <tangfdu@gmail.com>
2024-09-05 19:40:01 -0400

    2015-2024

--- code/sum_to_year.py
+++ code/sum_to_year.py
@@ -3,7 +3,7 @@
 import pandas as pd
 from pathlib import Path
 import time
-
+# %%
 # **Readme
 # This code is for summing the volumeTEU by year, conPanjivaId and shpPanjivaId to get monthly data
 # missing value convert to 999, drop rows with non numeric conPanjivaId and shpPanjivaId
@@ -15,102 +15,158 @@ import time
 #set timer
 start_time = time.time()
 #read the each csv file in the folder Processed_data/USImport
+directorys = ["/Users/qianqiantang/Desktop/panjiva-code-main/Processed_data/USImport/2015-2019", "/Users/qianqiantang/Desktop/panjiva-code-main/Processed_data/USImport/2020-2024"]
+for directory in directorys:
+    files = Path(directory).glob('*address.csv')
+    #List to store dataframes
+    fileData = []
+    #For each file in the folder
+    for file in files:
+        print(file)
+        #Read the file
+        df = pd.read_csv(file)
 
-directory = "/Users/qianqiantang/Desktop/panjiva-code-main/Processed_data/USImport"
-files = Path(directory).glob('*address.csv')
-
-
-#List to store dataframes
-fileData = []
-#For each file in the folder
-for file in files:
-    print(file)
-    #Read the file
-    df = pd.read_csv(file)
-
-    # drop rows with non numeric conPanjivaId and shpPanjivaId
-    df['conPanjivaId'] = df['conPanjivaId'].astype(str)
-    df['shpPanjivaId'] = df['shpPanjivaId'].astype(str)
-
-    df = df[df['conPanjivaId'].str.isnumeric()]
-    df = df[df['shpPanjivaId'].str.isnumeric()]
-
-     #change conPanjivaId, shpPanjivaId, year, month, day to int
-
-    df['conPanjivaId'] = pd.to_numeric(df['conPanjivaId'], errors='coerce').astype('Int64')
-    df['shpPanjivaId'] = pd.to_numeric(df['shpPanjivaId'], errors='coerce').astype('Int64')
-    
-    #change conPanjivaId, shpPanjivaId, year, month, day to int
-    df['year'] = pd.to_numeric(df['year'], errors='coerce').astype('Int64')
-    df['month'] = pd.to_numeric(df['month'], errors='coerce').astype('Int64')
-    df['day'] = pd.to_numeric(df['day'], errors='coerce').astype('Int64')
-
-    # drop if year month day is na/nan
-    df = df.dropna(subset=['year', 'month', 'day'])
-
-    #Add the file to the list
-    fileData.append(df)
-
-# concatenate all files in files
-importus = pd.concat(fileData)
-
-#store importus into importus_copy
-importus_copy = importus.copy()
-
-#####################################
-# calculate days between shipments
-#####################################
-# caculater the days from last shipment
-importus['date_str'] = importus['year'].astype(str) + importus['month'].astype(str).str.zfill(2) + importus['day'].astype(str).str.zfill(2)
-# Convert the combined string column to datetime
-importus['date'] = pd.to_datetime(importus['date_str'], format='%Y%m%d')
-
-# Drop the temporary 'date_str' column
-importus = importus.drop(columns=['date_str'])
-
-# Continue with the rest of your code
-importus = importus.sort_values(['conPanjivaId', 'shpPanjivaId', 'date'])
-importus['days_from_last_shipment'] = importus.groupby(['conPanjivaId', 'shpPanjivaId'])['date'].diff().dt.days
-
-# caculate the average days_from_last_shipment by year, conPanjivaId, shpPanjivaId
-# importus['days_from_last_shipment'] = importus['days_from_last_shipment'].fillna(999)
-importus['average_days_from_last_shipment'] = importus.groupby(['year', 'conPanjivaId', 'shpPanjivaId'])['days_from_last_shipment'].transform('mean')
-
-#drop the column days_from_last_shipment
-importus = importus.drop(columns=['days_from_last_shipment'])
+        # drop rows with non numeric conPanjivaId and shpPanjivaId
+        # df['conPanjivaId'] = df['conPanjivaId'].astype(str)
+        # df['shpPanjivaId'] = df['shpPanjivaId'].astype(str)
+        
+        # df = df[df['conPanjivaId'].str.isnumeric()]
+        # df = df[df['shpPanjivaId'].str.isnumeric()]
+        df = df[df['conPanjivaId'].astype(str).str.isnumeric() & df['shpPanjivaId'].astype(str).str.isnumeric()]
 
 
-#####################################
-# group by year
-#####################################
+        #change conPanjivaId, shpPanjivaId, year, month, day to int
 
-#sum volumeTEU by year, month, conPanjivaId and shpPanjivaId but also keep other columns
-# aggregate the volumeTEU by year, conName, conPanjivaId, shpPanjivaId, conCountry, shpCountry, shpmtDestination, conFullAddress, conRoute, conCity, conStateRegion, conPostalCode
-aggregated_df = importus.groupby(['year', 'conName', 'conPanjivaId', 'shpPanjivaId', 'conCountry', 'shpCountry', 'shpmtDestination', 'conFullAddress', 'conRoute', 'conCity', 'conStateRegion', 'conPostalCode']).agg({'volumeTEU': 'sum'}).reset_index()
-aggregated_df = aggregated_df.rename(columns={'volumeTEU': 'Total_teu'})
-importus = importus.merge(aggregated_df, on=['year', 'conName', 'conPanjivaId', 'shpPanjivaId', 'conCountry', 'shpCountry', 'shpmtDestination', 'conFullAddress', 'conRoute', 'conCity', 'conStateRegion', 'conPostalCode'], how='left')
+        # df['conPanjivaId'] = pd.to_numeric(df['conPanjivaId'], errors='coerce').astype('Int64')
+        # df['shpPanjivaId'] = pd.to_numeric(df['shpPanjivaId'], errors='coerce').astype('Int64')
+        
+        #change conPanjivaId, shpPanjivaId, year, month, day to int
+        # df['year'] = pd.to_numeric(df['year'], errors='coerce').astype('Int64')
+        # df['month'] = pd.to_numeric(df['month'], errors='coerce').astype('Int64')
+        # df['day'] = pd.to_numeric(df['day'], errors='coerce').astype('Int64')
 
-# duplicate into paris level and year level
-# importus = importus.drop(columns=['panjivaRecordId', 'date', 'month', 'day', 'volumeteu'])
-# importus = importus.drop_duplicates()
-# importus.head()
+        df[['conPanjivaId', 'shpPanjivaId', 'year', 'month', 'day']] = df[['conPanjivaId', 'shpPanjivaId', 'year', 'month', 'day']].apply(pd.to_numeric, errors='coerce')
+        # drop if year month day is na/nan
+        df.dropna(subset=['year', 'month', 'day'], inplace=True)
+
+        #Add the file to the list
+        fileData.append(df)
+
+    # concatenate all files in files
+    importus = pd.concat(fileData)
 
 
-# calculate the number of distinct shpPanjivaId for each conPanjivaId by year
-importus['num_distinct_shpPanjivaId'] = importus.groupby(['year', 'conPanjivaId'])['shpPanjivaId'].transform('nunique')
+    #####################################
+    # calculate days between shipments
+    #####################################
+    # caculater the days from last shipment
+    importus['date_str'] = importus['year'].astype(str) + importus['month'].astype(str).str.zfill(2) + importus['day'].astype(str).str.zfill(2)
+    # Convert the combined string column to datetime
+    importus['date'] = pd.to_datetime(importus['date_str'], format='%Y%m%d')
 
+    # Drop the temporary 'date_str' column
+    importus = importus.drop(columns=['date_str'])
 
-# change columns names to lower case
-importus.columns = importus.columns.str.lower()
-
-# only keep rows if concountry is United States or NaN/Na
-importus = importus[(importus['concountry'] == 'United States')| (importus['concountry'].isnull())]
-
-# export importus into csv file
-importus.to_csv('/Users/qianqiantang/Desktop/panjiva-code-main/Processed_data/USImport/monthly/USImport_annual_2015-2024.csv', index=False)
-
+    # save every year data into csv file separately
+    for year in importus['year'].unique():
+        importus_year = importus[importus['year'] == year]
+        importus_year.to_csv(f'/Users/qianqiantang/Desktop/panjiva-code-main/Processed_data/USImport/annual/USImport_{year}.csv', index=False)
 
 #calculate the time it takes to run the code
 print("--- %s seconds ---" % (time.time() - start_time))
 
+
 # %%
+#Packages needed to run code
+import pandas as pd
+from pathlib import Path
+import time
+import os
+# **Readme
+# This code is for summing the volumeTEU by year, conPanjivaId and shpPanjivaId to get monthly data
+
+#####################################
+# Read raw data
+#####################################
+
+#set timer
+start_time = time.time()
+#read the each csv file in the folder Processed_data/USImport
+
+directory = "/Users/qianqiantang/Desktop/panjiva-code-main/Processed_data/USImport/annual"
+#files = Path(directory).glob('USImport_*.csv')
+
+#List to store dataframes
+fileData = []
+fileData_truncted = []
+#For each file in the folder
+# List of years you want to read files for
+years = [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024]
+
+for y in years:
+    filename = f'USImport_{y}.csv'  # Assuming the files are in CSV format
+    filepath = os.path.join(directory, filename)
+    df_year = pd.read_csv(filepath)
+    #sum volumeTEU by year, month, conPanjivaId and shpPanjivaId but also keep other columns
+    # aggregate the volumeTEU by year, conName, conPanjivaId, shpPanjivaId, conCountry, shpCountry, shpmtDestination, conFullAddress, conRoute, conCity, conStateRegion, conPostalCode
+    aggregated_df_year = df_year.groupby(['conName', 'conPanjivaId', 'shpPanjivaId', 'conCountry', 'shpCountry', 'shpmtDestination', 'conFullAddress', 'conRoute', 'conCity', 'conStateRegion', 'conPostalCode']).agg({'volumeTEU': 'sum'}).reset_index()
+    aggregated_df_year = aggregated_df_year.rename(columns={'volumeTEU': 'Total_teu'})
+    df_year = df_year.merge(aggregated_df_year, on=['conName', 'conPanjivaId', 'shpPanjivaId', 'conCountry', 'shpCountry', 'shpmtDestination', 'conFullAddress', 'conRoute', 'conCity', 'conStateRegion', 'conPostalCode'], how='left')
+
+    # calculate the number of distinct shpPanjivaId for each conPanjivaId by year
+    df_year['num_distinct_shpPanjivaId'] = df_year.groupby(['conPanjivaId'])['shpPanjivaId'].transform('nunique')
+
+    # only keep conPanjivaId and shpPanjivaId and date
+    df_year_truncted = df_year[['conPanjivaId', 'shpPanjivaId', 'date', 'year']]
+    # ******** need to mention: duplicated rows are dropped ******
+    df_year_truncted = df_year_truncted.drop_duplicates()
+    fileData_truncted.append(df_year_truncted)
+
+    # duplicate into paris level and year level
+    df_year = df_year.drop(columns=['panjivaRecordId', 'date', 'month', 'day', 'volumeTEU'])
+    df_year = df_year.drop_duplicates()
+    df_year.head()
+
+    # export df_year to csv file
+    df_year.to_csv(f'/Users/qianqiantang/Desktop/panjiva-code-main/Processed_data/USImport/annual/USImport_{y}_index.csv', index=False)
+
+
+# concatenate all files in files
+importus_delay_days = pd.concat(fileData_truncted)
+
+# # in case the laptop cannot handle the data, save the data into csv file
+# importus_delay_days.to_csv('/Users/qianqiantang/Desktop/panjiva-code-main/Processed_data/USImport/annual/USImport_delay_days.csv', index=False)
+
+
+# # read csv file
+# importus_delay_days = pd.read_csv('/Users/qianqiantang/Desktop/panjiva-code-main/Processed_data/USImport/annual/USImport_delay_days.csv')
+
+importus_delay_days['date'] = pd.to_datetime(importus_delay_days['date'])
+# caculate the days from last shipment
+importus_delay_days = importus_delay_days.sort_values(['conPanjivaId', 'shpPanjivaId', 'date'])
+
+importus_delay_days['days_from_last_shipment'] = importus_delay_days.groupby(['conPanjivaId', 'shpPanjivaId'])['date'].diff().dt.days
+
+# caculate the average days_from_last_shipment by year, conPanjivaId, shpPanjivaId
+# importus['days_from_last_shipment'] = importus['days_from_last_shipment'].fillna(999)
+importus_delay_days['average_days_from_last_shipment'] = importus_delay_days.groupby(['year', 'conPanjivaId', 'shpPanjivaId'])['days_from_last_shipment'].transform('mean')
+
+#drop the column days_from_last_shipment
+importus_delay_days = importus_delay_days.drop(columns=['days_from_last_shipment'])
+# duplicate into paris level and year level 
+importus_delay_days = importus_delay_days.drop_duplicates()
+
+for y in years:
+    filename = f'USImport_{y}_index.csv'  # Assuming the files are in CSV format
+    filepath = os.path.join(directory, filename)
+    df_year = pd.read_csv(filepath)
+    # merge importus_two_index and importus_delay_days by 'year', 'conPanjivaId', 'shpPanjivaId'
+    importus = df_year.merge(importus_delay_days, on=['year', 'conPanjivaId', 'shpPanjivaId'], how='left')
+    # save the data into csv file
+    importus.to_csv(f'/Users/qianqiantang/Desktop/panjiva-code-main/Processed_data/USImport/annual/USImport_{y}_three_index.csv', index=False)
+
+#calculate the time it takes to run the code
+print("--- %s seconds ---" % (time.time() - start_time))
+
+# %% 
+
